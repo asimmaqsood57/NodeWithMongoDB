0b92be0eac8bf47362793e9f911d3143341c9631
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-08-07 06:56:41 +0500

    how to insert record into database

--- App.js
+++ App.js
@@ -4,14 +4,39 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+// const users = require("./users");
 
+const Users = require("./users");
 // mongodb+srv://new-user_31:<password>@cluster0.plewu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-mongoose
-  .connect(
-    "mongodb+srv://new-user_31:0LCMnJ764HlqoNkL@cluster0.plewu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/youtube", ///youtube database Name
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => {
-    console.log("db connected done");
-  });
+mongoose.connect(
+  "mongodb+srv://new-user_31:0LCMnJ764HlqoNkL@cluster0.plewu.mongodb.net/youtube?retryWrites=true&w=majority", ///youtube database Name
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+//   .then(() => {
+//     console.log("db connected done");
+//   });
+
+// Users.find({}, (err, users) => {
+//   if (err) {
+//     console.log(err);
+//   }
+
+//   console.log(users);
+// }); // null obj means return all data
+
+//inserting record into database
+const data = new Users({
+  _id: mongoose.Types.ObjectId(),
+  name: "Jawad",
+  email: "jawad@gmail.com",
+
+  address: "Rokhari",
+});
+
+data
+  .save()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => console.log(err));
