e0eb0d37b80205de528b8b5cb345fc6b1e4d1396
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-08-07 07:54:03 +0500

    post api done

--- App.js
+++ App.js
@@ -9,6 +9,10 @@ const mongoose = require("mongoose");
 const Users = require("./users");
 // mongodb+srv://new-user_31:<password>@cluster0.plewu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
+var bodyParser = require("body-parser");
+
+var jsonParser = bodyParser.json();
+
 mongoose.connect(
   "mongodb+srv://new-user_31:0LCMnJ764HlqoNkL@cluster0.plewu.mongodb.net/youtube?retryWrites=true&w=majority", ///youtube database Name
   { useNewUrlParser: true, useUnifiedTopology: true }
@@ -26,17 +30,57 @@ mongoose.connect(
 // }); // null obj means return all data
 
 //inserting record into database
-const data = new Users({
-  _id: mongoose.Types.ObjectId(),
-  name: "Jawad",
-  email: "jawad@gmail.com",
+// const data = new Users({
+//   _id: mongoose.Types.ObjectId(),
+//   name: "Jawad",
+//   email: "jawad@gmail.com",
 
-  address: "Rokhari",
+//   address: "Rokhari",
+// });
+
+// data
+//   .save()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => console.log(err));
+
+//fetching data from database through get api
+
+app.get("/users", (req, res) => {
+  Users.find()
+    .select("name")
+    .then((data) => {
+      // console.log(data);
+      res.json(data);
+    });
 });
 
-data
-  .save()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => console.log(err));
+//making post Api
+
+//1  Make post router
+//2 Add json body-parser  pakage
+//3 Apply Body-Parser
+//4 write code  for save data in mongodb
+//5 Test with post man
+
+app.post("/user", jsonParser, (req, res) => {
+  const data = new Users({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    email: req.body.email,
+    address: req.body.address,
+  });
+  data
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+});
+
+app.listen(80);
